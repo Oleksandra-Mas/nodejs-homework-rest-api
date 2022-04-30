4054a04952a7fd1e5042cd6b0c80c1c612deab8c
Oleksandra-Mas <sashamasyuk0702@gmail.com>
2022-04-30 21:05:39 +0200

    updated contact and created user schemas

--- service/schemas/contacts.js
+++ service/schemas/contacts.js
@@ -41,6 +41,10 @@ const contact = new Schema(
       favorite: {
         type: Boolean,
         default: false
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
       }
     },
     { versionKey: false, timestamps: true }
